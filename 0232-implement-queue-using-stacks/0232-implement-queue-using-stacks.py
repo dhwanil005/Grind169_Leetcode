afec7cf010824eb1f7919df9283fa756fa1d322a
dhwanil005 <55379353+dhwanil005@users.noreply.github.com>
2022-11-19 16:07:48 -0800

    Time: 32 ms (92.63%), Space: 13.9 MB (75.82%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,28 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        if not self.stack1:
+            self.front =x
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+      
+        
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if self.stack2:
+            return self.stack2[-1]
+        return self.front
+
+    def empty(self) -> bool:
+        return(not self.stack1 and not self.stack2)
+
+
