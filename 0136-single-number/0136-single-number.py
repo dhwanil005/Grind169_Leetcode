a1eb4f20165f992e5839e68e9608c652c6fe25aa
dhwanil005 <55379353+dhwanil005@users.noreply.github.com>
2022-11-28 17:26:40 -0800

    Time: 187 ms (77.92%), Space: 16.7 MB (84.61%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
         x = dict(collections.Counter(nums))
         for k,v in x.items():
             if v==1:
